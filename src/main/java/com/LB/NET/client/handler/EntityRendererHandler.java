deded446d5c57cfb13b9b65280348029734bd404
LB2345 <128509865+LB2345@users.noreply.github.com>
2023-08-31 19:51:51 +0300

    Update EntityRendererHandler.java

--- src/main/java/com/LB/NET/client/handler/EntityRendererHandler.java
+++ src/main/java/com/LB/NET/client/handler/EntityRendererHandler.java
@@ -2,9 +2,16 @@ package com.BubbleTeaM.NET.client.handler;
 
 import com.BubbleTeaM.NET.NETMod;
 import com.BubbleTeaM.NET.client.model.NETModelLayers;
+import com.BubbleTeaM.NET.client.render.NETBoatRenderer;
+import com.BubbleTeaM.NET.common.block.entity.NETBlockEntities;
+import com.BubbleTeaM.NET.common.block.entity.NETSignBlockEntity;
 import com.BubbleTeaM.NET.common.entity.NETBoat;
+import com.BubbleTeaM.NET.common.entity.NETEntities;
 import net.minecraft.client.model.BoatModel;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.event.EntityRenderersEvent;
@@ -22,5 +29,10 @@ public class EntityRendererHandler
         for (NETBoat.Type type : NETBoat.Type.values()) {
             ForgeHooksClient.registerLayerDefinition(NETModelLayers.createNETBoatModelName(type), () -> boatLayerDefinition);
         }
+        // Register block entity renderers
+        event.registerBlockEntityRenderer((BlockEntityType<NETSignBlockEntity>)NETBlockEntities.SIGN_BLOCK_ENTITIES.get(), SignRenderer::new);
+
+        // Register entity renderers
+        event.registerEntityRenderer((EntityType<NETBoat>) NETEntities.BOAT.get(), NETBoatRenderer::new);
     }
 }
