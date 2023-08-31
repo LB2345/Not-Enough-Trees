59d99ef05977f3f99095021184299f05f1f6dbea
LB2345 <128509865+LB2345@users.noreply.github.com>
2023-08-31 19:25:40 +0300

    Added Endburst WoodType

--- src/main/java/com/LB/NET/common/block/entity/NETWoodTypes.java
+++ src/main/java/com/LB/NET/common/block/entity/NETWoodTypes.java
@@ -4,4 +4,5 @@ import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class NETWoodTypes {
     public static WoodType POPLAR = WoodType.create("poplar");
+    public static WoodType ENDBURST = WoodType.create("endburst");
 }
