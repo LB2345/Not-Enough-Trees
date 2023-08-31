838d48cb776b90ca8a694be659ecf76b059c118f
LB2345 <128509865+LB2345@users.noreply.github.com>
2023-08-31 19:23:37 +0300

    Update NETBoatRenderer.java

--- src/main/java/com/LB/NET/client/render/NETBoatRenderer.java
+++ src/main/java/com/LB/NET/client/render/NETBoatRenderer.java
@@ -33,6 +33,51 @@ public class NETBoatRenderer extends EntityRenderer<NETBoat> {
         }));
     }
 
+    public void render(NETBoat p_113929_, float p_113930_, float p_113931_, PoseStack p_113932_, MultiBufferSource p_113933_, int p_113934_) {
+        p_113932_.pushPose();
+        p_113932_.translate(0.0D, 0.375D, 0.0D);
+        p_113932_.mulPose(Vector3f.YP.rotationDegrees(180.0F - p_113930_));
+        float f = (float)p_113929_.getHurtTime() - p_113931_;
+        float f1 = p_113929_.getDamage() - p_113931_;
+        if (f1 < 0.0F) {
+            f1 = 0.0F;
+        }
+
+        if (f > 0.0F) {
+            p_113932_.mulPose(Vector3f.XP.rotationDegrees(Mth.sin(f) * f * f1 / 10.0F * (float)p_113929_.getHurtDir()));
+        }
+
+        float f2 = p_113929_.getBubbleAngle(p_113931_);
+        if (!Mth.equal(f2, 0.0F)) {
+            p_113932_.mulPose(new Quaternion(new Vector3f(1.0F, 0.0F, 1.0F), p_113929_.getBubbleAngle(p_113931_), true));
+        }
+
+        Pair<ResourceLocation, BoatModel> pair = getModelWithLocation(p_113929_);
+        ResourceLocation resourcelocation = pair.getFirst();
+        BoatModel boatmodel = pair.getSecond();
+        p_113932_.scale(-1.0F, -1.0F, 1.0F);
+        p_113932_.mulPose(Vector3f.YP.rotationDegrees(90.0F));
+        boatmodel.setupAnim(p_113929_, p_113931_, 0.0F, -0.1F, 0.0F, 0.0F);
+        VertexConsumer vertexconsumer = p_113933_.getBuffer(boatmodel.renderType(resourcelocation));
+        boatmodel.renderToBuffer(p_113932_, vertexconsumer, p_113934_, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        if (!p_113929_.isUnderWater()) {
+            VertexConsumer vertexconsumer1 = p_113933_.getBuffer(RenderType.waterMask());
+            boatmodel.waterPatch().render(p_113932_, vertexconsumer1, p_113934_, OverlayTexture.NO_OVERLAY);
+        }
+
+        p_113932_.popPose();
+        super.render(p_113929_, p_113930_, p_113931_, p_113932_, p_113933_, p_113934_);
+    }
+
+    @Deprecated
+    public ResourceLocation getTextureLocation(NETBoat p_113927_) {
+        return getModelWithLocation(p_113927_).getFirst();
+    }
+
+    public Pair<ResourceLocation, BoatModel> getModelWithLocation(NETBoat boat) {
+        return this.boatResources.get(boat.getModBoatType()); }
+}
+
     @Deprecated // forge: override getModelWithLocation to change the texture / model
     public ResourceLocation getTextureLocation(NETBoat p_113927_) {
         return getModelWithLocation(p_113927_).getFirst();
